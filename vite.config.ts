9f09c4754f41d433ee00ad7b6c21b9b87180e2cb
Papalexios <66846784+Papalexios@users.noreply.github.com>
2025-08-29 13:29:35 +0300

    Fix XML format error by enabling proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // server: {
-      //   proxy: {
-      //     '/wp-sitemap-proxy': {
-      //       target: 'https://example-wordpress-site.com', // Update this to your WordPress site URL
-      //       changeOrigin: true,
-      //       rewrite: (path) => path.replace(/^\/wp-sitemap-proxy/, '')
-      //     }
-      //   }
-      // },
+      server: {
+        proxy: {
+          '/wp-sitemap-proxy': {
+            target: 'https://example-wordpress-site.com', // Update this to your WordPress site URL
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/wp-sitemap-proxy/, '')
+          }
+        }
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
